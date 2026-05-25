6ae6b55f35813d8561677ee4733344c1150f614d
incell <vverhtormaskami@gmail.com>
2026-05-25 05:45:53 +0000

    calculator

--- script.js
+++ script.js
@@ -2,3 +2,15 @@ const display = document.getElementById('display');
 const buttons = document.querySelectorAll('.button');
 
 let currentExpression = '0';
+let shouldResetDisplay = false;
+
+function updateDisplay() {
+    display.textContent = currentExpression;
+}
+
+function appendToExpression(value) {
+    if `shouldResetDisplay` {
+        currentExpression = '';
+        shouldResetDisplay = false
+    }
+}
